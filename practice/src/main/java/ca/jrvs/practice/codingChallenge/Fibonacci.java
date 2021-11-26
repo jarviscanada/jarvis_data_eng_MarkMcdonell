4f8267f47f854d682e5a9bc87610be5af0c1b8dc
Mark Mcdonell <mark.mcdonell22@gmail.com>
2021-11-26 16:21:48 -0500

    fixed coding challenges

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/Fibonacci.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/Fibonacci.java
@@ -1,2 +1,18 @@
-package ca.jrvs.practice.codingChallenge;public class Fibonacci {
+package ca.jrvs.practice.codingChallenge;
+public class Fibonacci {
+  //bottom up approach has best performance
+  public int fib(int n) {
+    if (n == 0) {
+      return 0;}
+    if (n == 1 || n == 2) {
+      return 1;}
+    int[] bottom_up = new int[n+1];
+    bottom_up[0]=0;
+    bottom_up[1]=1;
+    bottom_up[2]=1;
+    for (int i = 3 ; i <= n ; i++){
+      bottom_up[i] = bottom_up[i-1] + bottom_up[i-2];
+    }
+    return bottom_up[n];
+  }
 }
